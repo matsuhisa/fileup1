0487a36a9915ed339eb917411a45ce8a4312397c
matsuhisa <matsuhisa+github@gmail.com>
2017-07-30 14:48:06 +0900

    rails g scaffold Prefecture name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730054315) do
+ActiveRecord::Schema.define(version: 20170730054613) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
     t.integer "prefecture"
     t.string "municipality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ test/system/prefectures_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class PrefecturesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit prefectures_url
+  #
+  #   assert_selector "h1", text: "Prefecture"
+  # end
+end
