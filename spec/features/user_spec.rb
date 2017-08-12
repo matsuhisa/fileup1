e6544195941209b9c629b73397c00b2f415cda6b
matsuhisa <matsuhisa+github@gmail.com>
2017-08-12 17:40:19 +0900

    Fix spec/features/user_spec.rb

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'ユーザページ', type: :feature do
-  context 'indexにアクセスする' do
-
-    scenario '' do
-      visit users_path
-      expect(page).to have_title 'Fileup1'
-    end
+  scenario 'indexにアクセスする' do
+    visit users_path
+    expect(page).to have_title 'Fileup1'
   end
 end
