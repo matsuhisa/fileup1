4932e10cc118664892649484d59ad44255a7dc38
matsuhisa <matsuhisa+github@gmail.com>
2017-08-16 06:42:51 +0900

    WIP

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -40,4 +40,28 @@ RSpec.feature 'ユーザページ', type: :feature do
     user = User.find_by(name: '佐藤')
     expect(user.post_images.size).to eq 2
   end
+
+  scenario 'ファイルアップロードと再入力がある場合', js: true do
+    visit users_path
+    expect(page).to have_title 'Fileup1'
+
+    click_on 'New User'
+    expect(page).to have_content '新規ユーザー登録'
+
+    fill_in 'user_registration_form[age]', with: '20'
+    fill_in 'user_registration_form[addresses_attributes][0][municipality]', with: '帯広市'
+
+    attach_file 'user_registration_form[post_images_attributes][0][file_name]', "#{Rails.root}/spec/images/octocat.png"
+    attach_file 'user_registration_form[post_images_attributes][1][file_name]', "#{Rails.root}/spec/images/octocat.png"
+    click_button 'Save'
+
+    # 保存されない
+    expect(page).to have_content '新規ユーザー登録'
+    fill_in 'user_registration_form[name]', with: '田中'
+    click_button 'Save'
+
+    expect(page).to have_content 'User was successfully'
+    user = User.find_by(name: '田中')
+    expect(user.post_images.size).to eq 2
+  end
 end
