87b0d071fa21712f8644569ac89a3a2ee25731a4
matsuhisa <matsuhisa+github@gmail.com>
2017-08-20 17:03:15 +0900

    Remove destory, update from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit]
 
   def index
     @users = User.all
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user_register_form = User::RegistrationForm.new
   end
 
-  def edit
+  def confirm
   end
 
   def create
@@ -27,34 +27,17 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
-    # @user.addresses = @user.addresses.reject { |item| item.municipality.blank? }
-    # respond_to do |format|
-    #   if @user.update(user_params)
-    #     format.html { redirect_to @user, notice: 'User was successfully updated.' }
-    #     format.json { render :show, status: :ok, location: @user }
-    #   else
-    #     format.html { render :edit }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
+  def complete
   end
 
-  def destroy
-    # @user.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-    #   format.json { head :no_content }
-    # end
+  def edit
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user_registration_form).permit(
         :name, :age, 
